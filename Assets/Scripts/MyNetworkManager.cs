2b48862c6e056bbbf3e95acb4b660ab8c75072aa
Wigmarr <lorddimon2010@gmail.com>
2021-05-13 13:39:37 +0300

    project files

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnStartServer() {
+        Debug.Log("Server started!");        
+    }
+
+    public override void OnStopServer() {
+        Debug.Log("Server stopped!");
+    }
+    public override void OnClientConnect(NetworkConnection conn) {
+        Debug.Log("Player connected!");        
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn) {
+        Debug.Log("Playr disconnected");
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,13 @@
-﻿using Mirror;
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
+    
+    [SyncVar(hook = nameof(OnHolaCountChange))]
+    int holaCount = 0;
     void HandleMovement() {
         if (isLocalPlayer) {
             float moveHorizontal = Input.GetAxis("Horizontal");
@@ -13,8 +16,34 @@ public class Player : NetworkBehaviour
             transform.position = transform.position + movement;
         }
     }
-
+    
     void Update() {
         HandleMovement();
+
+        if (isLocalPlayer && Input.GetKeyDown("x")) {
+            Debug.Log("Sending Hola to Server");
+            Hola();
+        }
+    }
+
+    [Command]
+    void Hola() {
+        Debug.Log("Received Hola from Client~!");
+        holaCount++;
+        ReplyHola();
+    }
+
+    [TargetRpc]
+    void ReplyHola() {
+        Debug.Log("Received Hola form Server!");
+    }
+    
+    [ClientRpc]
+    void TooHigh() {
+        Debug.Log("Too high!");
+    }
+
+    void OnHolaCountChange(int oldCount, int newCount) {
+        Debug.Log($"We had {oldCount} holas, but now we have {newCount}");
     }
 }
